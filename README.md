fb78c8172b98ae31338dd3a3fa2252aaf85a65f1
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-05-13 21:56:21 -0500

    print path in 2D grid

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Amazing
 
 a terminal maze generator
+
+* get dimensions
+* create a box of those dimensions
+* trace a route through the box from top to bottom
+  * it should be somewhat erratic
+  * it should not cross itself
+* using the route, fill in walls elsewhere
+
+
--- amazing/main.cpp
+++ amazing/main.cpp
@@ -11,5 +11,27 @@
 int main(int argc, const char * argv[])
 {
   std::cout << "This will be Amazing." << std::endl;
+  /*
+   * Given a multi-dimensional array representing a path in a grid,
+   * print the path.
+   */
+  int array[5][5] =
+  {
+    { 0, 1, 0, 0, 0 },
+    { 0, 1, 1, 1, 0 },
+    { 0, 0, 0, 1, 0 },
+    { 0, 1, 1, 1, 0 },
+    { 0, 1, 0, 0, 0 },
+  };
+
+  for (int row = 0; row < 5; row++)
+  {
+    for (int column = 0; column < 5; column++)
+    {
+      int cell = array[row][column];
+      std::cout << (cell == 1 ? "*" : " ");
+    }
+    std::cout << std::endl;
+  }
   return 0;
 }
